96794d73ca0cbbe0f0a30c24c0ea972beec6ee45
Megan G <meganelizabethgoode@hotmail.com>
2024-03-27 17:35:01 +0000

    Added 'addnewplayer' and lobby functionality

--- nabegemu/Program.cs
+++ nabegemu/Program.cs
@@ -1,4 +1,5 @@
 using nabegemu.Components;
+using nabegemu.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +24,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapHub<LobbyHub>("/lobbyhub");
 
 app.Run();
